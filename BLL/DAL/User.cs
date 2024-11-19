16d8c7c701db79572bbc73af7ce1078c76f01cb6
progman <buseclkcxx@gmail.com>
2024-11-19 23:05:46 +0300

    Role and User classes added

--- BLL/DAL/User.cs
+++ BLL/DAL/User.cs
@@ -4,9 +4,9 @@ namespace BLL.DAL
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; } 
         public bool IsActive { get; set; }
         public int RoleId { get; set; }
-        public Role Role { get; set; }
+        public string Role { get; set; } = string.Empty; 
     }
 }
